1f435be3d97074cbc9d1754c01ab34dfbbe822ac
etherio97 <ethereal97@gmail.com>
2022-01-02 19:16:50 +0630

    add selected database into localstorage

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const auth = firebase.auth(),
   db = firebase.database(),
   s3 = firebase.storage();
 
+const ls = {
+  _dbKey: '__ls:db',
+  _fallback: undefined,
+  get db() {
+    return unserialize(localStorage.getItem(this._dbKey) || this._fallback);
+  },
+  set db(value) {
+    localStorage.setItem(this._dbKey, serialize(value));
+  },
+  clearDb() {
+    localStorage.clear();
+  },
+};
+
 new Vue({
   data: {
     page: 'user_profile',
@@ -19,6 +33,7 @@ new Vue({
       databaseUrl: '',
       secretToken: '',
     },
+    selectedDb: undefined,
   },
   methods: {
     handleAuth(userOrNull) {
@@ -26,65 +41,77 @@ new Vue({
       this.currentUser = userOrNull;
       this.loggedIn && this.refreshToken();
     },
-    signIn() {
-      const { email, password } = this.login;
-      this.login.error = '';
-      auth.signInWithEmailAndPassword(email, password)
-        .catch(err => {
-          this.login.error = err.message;
-        })
+    async signIn() {
+      this.login.error = '';  
+      try {
+        const { email, password } = this.login;
+        await auth
+          .signInWithEmailAndPassword(
+            email,
+            password
+          );
+      } catch (e) {
+        this.login.error = e.message;
+      }
     },
     signOut() {
-      auth.signOut();
+      return auth.signOut();
     },
     async refreshToken() {
-      this.accessToken = await auth.currentUser.getIdToken();
+      this.accessToken = await auth
+        .currentUser
+        .getIdToken();
     },
     selectAll(event) {
       event.target.select();
     },
-    fetchDbs() {
-      this.page = 'my_database';
-      db.ref('users')
+    async fetchDbs() {
+      const ss = await db.ref('users')
         .child(this.currentUser.uid)
         .child('dbs')
-        .get()
-        .then(data => {
-          this.dbs = Object.entries(
-            data.val() || {}
-          ).map(
-            ([id, value]) => ({
-              id, _displayed: false, ...value
-            })
-          ).reverse();
-        });
+        .get();
+      this.selectedDb = ls.db;
+      this.page = 'my_database';
+      this.dbs = Object
+        .entries(ss.val() || {})
+        .map(
+          ([id, value]) => ({
+            id,
+            ...value,
+            _displayed: false
+          })).reverse();
     },
     reloadDb() {
       return this.fetchDbs();
     },
-    createDb() {
-      db.ref('users')
+    async createDb() {
+      const created = this.created;
+      await testConnection(created);
+      await db.ref('users')
         .child(this.currentUser.uid)
         .child('dbs')
-        .push({ 
-          ...this.created,
-          createdAt: firebase.database.ServerValue.TIMESTAMP,
-        })
-        .then(() => {
-          this.created.databaseUrl = '';
-          this.created.secretToken = '';
-          this.reloadDb();
-        });
+        .push({ ...created, createdAt: firebase.database.ServerValue.TIMESTAMP });
+      this.created.databaseUrl = '';
+      this.created.secretToken = '';
+      this.reloadDb();
     },
-    deleteDb({ id }) {
+    async deleteDb({ id }) {
       if (!confirm('Are you sure to delete this database?')) return;
-      db.ref('users')
+      await db.ref('users')
         .child(this.currentUser.uid)
         .child('dbs')
         .child(id)
-        .remove()
-        .then(() => this.reloadDb())
+        .remove();
+      this.reloadDb();
     },
+    useDb(selected) {
+      if (!confirm('Are you sure to use this database?')) return;
+      this.selectedDb = ls.db = selected;
+    },
+    clearDb() {
+      ls.clearDb();
+      this.selectedDb = ls.clearDb();
+    }
   },
   computed: {
     decodedId() {
@@ -105,9 +132,32 @@ new Vue({
     auth.onAuthStateChanged(
       this.handleAuth.bind(this)
     );
-
     setInterval(() => {
       this.currentDate = new Date();
     }, 1000);
+    // setTimeout(()=>this.reloadDb(), 800);
   },
 }).$mount('#app');
+
+function testConnection({ databaseUrl, secretToken }) {
+  const url = new URL(databaseUrl);
+  url.searchParams.append('auth', secretToken);
+  return axios.get(url.toString()).then(({ status, data }) => {
+    if (status !== 200) {
+      throw new Error(`Invalid HTTP Status: ${status}`);
+    }
+    return data;
+  });
+}
+
+function serialize(obj) {
+  return JSON.stringify(obj);
+}
+
+function unserialize(raw) {
+  try {
+    return JSON.parse(raw);
+  } catch (e) {
+    return raw;
+  }
+}
